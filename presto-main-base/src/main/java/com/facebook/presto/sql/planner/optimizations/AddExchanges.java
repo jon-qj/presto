e6a3d72e7644a29f3dc2d90d46af820bb0589cde
aditi-pandit <Aditi.Pandit@ibm.com>
2025-11-06 14:33:49 -0800

    feat(native): Add TopNRowNumberNode rank function plumbing to Velox

--- presto-main-base/src/main/java/com/facebook/presto/sql/planner/optimizations/AddExchanges.java
+++ presto-main-base/src/main/java/com/facebook/presto/sql/planner/optimizations/AddExchanges.java
@@ -514,6 +514,7 @@ public class AddExchanges
                                 idAllocator.getNextId(),
                                 child.getNode(),
                                 node.getSpecification(),
+                                node.getRankingFunction(),
                                 node.getRowNumberVariable(),
                                 node.getMaxRowCountPerPartition(),
                                 true,
